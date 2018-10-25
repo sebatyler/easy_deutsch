5b2d223a439546d8b2a96f38a32e0c1a0cc93c89
sebatyler <sebatyler@gmail.com>
2018-10-25 16:47:41 +0300

    Use dict.leo.org instead of dict.cc

--- easy_deutsch/views.py
+++ easy_deutsch/views.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 
 from django.shortcuts import render
 
+from py_translator import Translator
+
 patterns = [re.compile(f"var c{i}Arr = new Array\((.*)\);")
             for i in range(1, 3)]
 
@@ -18,58 +20,40 @@ def get_context(word):
     headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) "
                              "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36"}
 
-    # https://www.dict.cc/?s=datei
-    res = requests.get('https://www.dict.cc', params=dict(s=word), headers=headers)
+    res = requests.get(f'https://dict.leo.org/englisch-deutsch/{word}', headers=headers)
     soup = BeautifulSoup(res.content, 'html.parser')
 
-    # TODO: ADJ/ADV
-    article = soup.select_one('tr[title="article sg | article pl"]')
+    result = soup.select_one('div[data-dz-search="result"]')
 
-    if not article:
-        # suggestion
-        suggestions = None
-        for bold in soup.select('td.td2 > b'):
-            if 'German Suggestions' in bold.text:
-                suggestions = pydash.map_(bold.parent.parent.parent.select('a[rel=nofollow]'),
-                                          lambda a: dict(word=a.text, link=f"?word={quote_plus(a.text)}"))
-                break
+    # TODO: suggestion
 
-        return dict(suggestions=suggestions)
+    search_term = result.get('data-leo-search-term') or word
+    word_info = dict(word=search_term, en=[], de=[])
 
-    article_text = article.text.replace('edit', '').strip()
-    word_info = dict(
-        word=word,
-        info=[
-            article_text,
-            *pydash.map_(article.next_siblings, lambda s: s.text.replace('edit', '').strip())
-        ]
-    )
+    tables = result.select('div > table')
 
-    soup = BeautifulSoup(res.content, 'html.parser')
+    for entry in tables[0].select('tbody > tr[data-dz-ui="dictentry"]'):
+        for lang in ('en', 'de'):
+            data = entry.select_one(f'td[lang="{lang}"]')
+            if data:
+                word_info[lang].append(data.text)
 
-    for script in soup.find_all('script'):
-        script_str = str(script.string)
-        match = patterns[0].search(script_str)
-        if match:
-            for lang, pattern in [
-                ('english', patterns[0]),
-                ('deutsch', patterns[1])
-            ]:
-                match = pattern.search(script_str)
-                if not match:
-                    break
+    # example sentences
+    for entry in result.select('[data-dz-name="example"] > table > tbody > tr[data-dz-ui="dictentry"]'):
+        for lang in ('en', 'de'):
+            data = entry.select_one(f'td[lang="{lang}"]')
+            if data:
+                word_info[lang].append(data.text)
 
-                word_info[lang] = pydash.chain(match.groups()[0].split(',')).map(
-                    lambda x: x[1:-1]  # remove quote
-                ).compact().take(5).value()
+    # korean
+    if word_info['en']:
+        korean = Translator().translate(text=word_info['en'][0], dest='ko')
+        word_info['ko'] = [korean.text]
 
-            if 'english' in word_info:
-                break
-
+    # TODO: search_term에 article 붙이기
     # https://pixabay.com/api/docs/
-    q = ' '.join(article_text.split('|')[0].split()[1:])
     res = requests.get('https://pixabay.com/api/',
-                       params=dict(key='10332400-1448498582be2b2e5a39c04ca', q=q, lang='de', per_page=12))
+                       params=dict(key='10332400-1448498582be2b2e5a39c04ca', q=search_term, lang='de', per_page=12))
     word_info['images'] = pydash.map_(res.json()['hits'],
                                       lambda x: dict(preview=x['previewURL'], large=x['largeImageURL']))
 
