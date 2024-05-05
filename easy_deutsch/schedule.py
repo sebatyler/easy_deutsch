02aca7c488b4fcb0d8f48d53c285605cf0dc3946
sebatyler <sebatyler@gmail.com>
2024-05-05 16:45:40 +0900

    feat: add send_random_vocabulary

--- easy_deutsch/schedule.py
+++ easy_deutsch/schedule.py
@@ -1,7 +1,11 @@
+import re
 from os import environ
+from random import choice
 
 import dotenv
+import gspread
 import telegram
+from oauth2client.service_account import ServiceAccountCredentials
 
 from django.conf import settings
 
@@ -9,10 +13,41 @@ from easy_deutsch.models import get_random_row
 
 dotenv.read_dotenv(settings.BASE_DIR / ".env")
 
+bot = telegram.Bot(environ["TELEGRAM_BOT_TOKEN"])
+
+
+def send_mesage(text, **kwargs):
+    return bot.sendMessage(chat_id=environ["TELEGRAM_BOT_CHANNEL_ID"], text=text, **kwargs)
+
 
 def send_random():
     row = get_random_row()
     sentences = "\n".join([*row["rest"], row["de"]])  # en, ..., de
 
-    bot = telegram.Bot(environ["TELEGRAM_BOT_TOKEN"])
-    print(bot.sendMessage(chat_id=environ["TELEGRAM_BOT_CHANNEL_ID"], text=sentences))
+    send_mesage(sentences)
+
+    send_random_vocabulary()
+
+
+def send_random_vocabulary():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("easy-deutsch.json", scope)
+    gc = gspread.authorize(credentials)
+    sheet = gc.open_by_key("1MLrRGwww30yjqpLA32R4Nbplq0N0HkwHyus3x9hd55A").worksheet("Vocabulary")
+
+    # skip the first row
+    rows = sheet.get_all_values()[1:]
+
+    # choose a random vocabulary
+    row = choice(rows)
+    title = f"Vocabulary: *{row[0]}*"
+    if row[1]:
+        title += f" ({row[1]})"
+    title = re.sub(r"([\-.()])", r"\\\1", title)
+
+    conversation = f"{row[2]}\n{row[3]}"
+
+    send_mesage(
+        f"{title}\n```\n{conversation}```",
+        parse_mode=telegram.ParseMode.MARKDOWN_V2,
+    )
